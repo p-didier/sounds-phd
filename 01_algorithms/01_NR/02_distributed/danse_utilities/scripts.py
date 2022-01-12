544ab90d0f33e65096851cc4e1973b85eed5ecee
Paul Didier <pdidier@sista-nc-1.esat.kuleuven.be>
2022-01-12 13:26:25 +0100

    Prepared environment for DANSE implementation

---
+++ 01_algorithms/01_NR/02_distributed/danse_utilities/scripts.py
@@ -0,0 +1,20 @@
+from . import classes
+
+def runExperiment(settings):
+    """Runs an experiment given certain settings.
+    Parameters
+    ----------
+    settings : classes.ProgramSettings object.
+        The settings for the current run.
+
+    Returns
+    -------
+    results : XXXX
+        The experiment results.
+    """
+
+    results = classes.Results()
+
+    
+
+    return results
