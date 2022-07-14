f28e775de1f3983c911045c3a0224ab5dd6318a3
p-didier <paul.didier@esat.kuleuven.be>
2022-07-14 16:29:24 +0200

    More efficient -- convolve just the last sample

--- 97_tests/05_dsp_related/202207_wola_filterbank/core/utils.py
+++ 97_tests/05_dsp_related/202207_wola_filterbank/core/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.signal as sig
 import matplotlib.pyplot as plt
 import copy
 from dataclasses import dataclass
@@ -25,18 +26,21 @@ def wola_fd_broadcast(x, Nh, R, w):
     """
 
     h = np.sqrt(np.hanning(Nh))
-    f = copy.copy(h)    # <-- synthesis window is the same as the analysis window
+    # f = copy.copy(h)    # <-- synthesis window is the same as the analysis window
 
     nframes = len(x) // R 
     
     z = np.zeros((Nh, nframes), dtype=complex)
     ii = 0
+    chunks = np.zeros((nframes, Nh))
     while True:
         idxBeg = ii*R
         idxEnd = idxBeg + Nh
         if idxEnd > len(x):
             break
         xcurr = copy.copy(x[idxBeg:idxEnd])
+
+        chunks[ii, :] = xcurr
 
         xcurr *= h  # synthesis window
 
@@ -55,26 +59,127 @@ def wola_fd_broadcast(x, Nh, R, w):
 
         ii += 1
 
-    return z
+    return z, chunks
 
 
-def plotit(x, y, w):
+def wola_td_broadcast(x, Nh, w, updateFilterEveryXsamples=1):
+    """
+    Parameters
+    ----------
+    x : np.ndarray
+        Input signal (1-dimensional).
+    Nh : int
+        Window length. 
+    w : np.ndarray
+        Filter coefficients in the frequency domain.
+    updateFilterEveryXsamples : int
+        Number of samples in-between consecutive time-domain filter updates.
+    """
 
-    fig, axes = plt.subplots(3,1)
+    h = np.sqrt(np.hanning(Nh))
+    f = copy.copy(h)    # <-- synthesis window is the same as the analysis window
+
+    # Exponential complex factor (typical 'FFT' notation `W`)
+    W = np.exp(1j * 2 * np.pi / Nh) 
+
+    lastUpdate = -1
+    ztd = np.zeros_like(x)
+    t = np.zeros(Nh)
+    t[0] = 1    # initialize impulse responde as Dirac
+    for n in range(len(x)):     # loop over samples
+        print(f'Sample {n+1}/{len(x)}')
+
+        # Update filter
+        if (n - lastUpdate) >= updateFilterEveryXsamples:
+            # Build sum
+            T = 0
+            terms = np.zeros((Nh, len(h)), dtype=complex)
+            for kappa in range(Nh):
+
+                # w[kappa] = 1
+
+                term = np.fft.fft(W ** (kappa * np.arange(len(h))) * h, Nh, axis=0)\
+                    * w[kappa]\
+                    * np.fft.fft(W ** (kappa * np.arange(len(h))) * f, Nh, axis=0)
+
+                terms[kappa, :] = term
+
+                # if kappa == Nh - 1:
+                #     fig, axes = plt.subplots(1,1)
+                #     fig.set_size_inches(6.5, 2.5)
+                #     axes.plot(20*np.log10(np.abs(terms.T)), label='individual channel filters')
+                #     # axes.plot(np.sum(20*np.log10(np.abs(terms)), axis=0), 'k')
+                #     axes.plot(20*np.log10(np.abs(T)), 'k', label='fullband filter')
+                #     axes.plot(20*np.log10(np.abs(w)), 'r--', label='spectral modifications')
+                #     axes.grid()
+                #     axes.set_ylim([-50, 100])
+                #     axes.set_title(f'Sample #{n+1}')
+                #     plt.tight_layout()	
+                #     plt.show()
+
+                #     stop = 1
+
+                T += term
+            T /= Nh
+            # Time-domain version of distortion filter
+            t = np.fft.ifft(T, Nh, axis=0)
+            t = np.real_if_close(t)
+            lastUpdate = n
+
+        # Perform convolution
+        if n < Nh:
+            currChunk = np.concatenate((np.zeros(Nh - n), x[:n]))
+        else:
+            currChunk = x[(n - Nh):n]
+
+        # # Convolve
+        # tmp = sig.convolve(currChunk, t, mode='valid')
+        # ztd[n] = tmp[-1]
+        # Convolve (manually, just the last sample)
+        ztd[n] = sum(currChunk * np.flip(t))
+
+    return ztd
+
+
+def plotit(z, z_fromtd, idx, unwrapPhase=False):
+
+    fig, axes = plt.subplots(2,1)
     fig.set_size_inches(12.5, 3.5)
-    axes[0].plot(x, label='x')
-    axes[0].plot(y, label='y')
+    axes[0].plot(20*np.log10(np.abs(z[:, idx])), label='From FD')
+    axes[0].plot(20*np.log10(np.abs(z_fromtd[:, idx])), label='From TD')
     axes[0].legend()
     axes[0].grid()
+    axes[0].set_title('Magnitude [dB]')
     # Filter coefficients
-    axes[1].plot(np.real(w), 'k', label='Re{w}')
-    axes[1].plot(np.imag(w), 'r', label='Im{w}')
+    if not unwrapPhase:
+        axes[1].plot(np.angle(z[:, idx]), label='From FD')
+        axes[1].plot(np.angle(z_fromtd[:, idx]), label='From TD')
+        axes[1].set_title('Phase angle [rad]')
+    else:
+        axes[1].plot(np.unwrap(np.angle(z[:, idx])), label='From FD')
+        axes[1].plot(np.unwrap(np.angle(z_fromtd[:, idx])), label='From TD')
+        axes[1].set_title('Phase angle [rad] (unwrapped)')
     axes[1].legend()
     axes[1].grid()
-    # Spectra
-    axes[2].plot(20*np.log10(np.abs(np.fft.fft(x))), label='X')
-    axes[2].plot(20*np.log10(np.abs(np.fft.fft(y))), label='Y')
-    axes[2].legend()
-    axes[2].grid()
     plt.tight_layout()	
     plt.show()
+# def plotit(x, y, w):
+
+#     fig, axes = plt.subplots(3,1)
+#     fig.set_size_inches(12.5, 3.5)
+#     axes[0].plot(x, label='x')
+#     axes[0].plot(y, label='y')
+#     axes[0].legend()
+#     axes[0].grid()
+#     # Filter coefficients
+#     axes[1].plot(np.real(w), 'k', label='Re{w}')
+#     axes[1].plot(np.imag(w), 'r', label='Im{w}')
+#     axes[1].legend()
+#     axes[1].grid()
+#     # Spectra
+#     axes[2].plot(20*np.log10(np.abs(np.fft.fft(x))), label='X')
+#     axes[2].plot(20*np.log10(np.abs(np.fft.fft(y))), label='Y')
+#     axes[2].legend()
+#     axes[2].grid()
+#     plt.tight_layout()	
+#     plt.show()
--- 97_tests/05_dsp_related/202207_wola_filterbank/script.py
+++ 97_tests/05_dsp_related/202207_wola_filterbank/script.py
@@ -2,9 +2,10 @@ import sys
 import core.utils as c
 import soundfile as sf
 import numpy as np
+import copy
 
 p = c.Params(
-    T=2,    # signal duration [s]
+    T=1,    # signal duration [s]
     Nh=512, # analysis/synthesis windows size [samples]
     R=256,  # window shift [samples]
     seed=12345, # random generator seed
@@ -28,14 +29,49 @@ def main():
     w = np.concatenate((w, np.flip(w[:-1, :].conj(), axis=0)[:-1, :]), axis=0)  # make symmetric
     w = np.squeeze(w)
 
+    # w = np.ones_like(w)
+
     # Run WOLA
-    z = c.wola_fd_broadcast(x, p.Nh, p.R, w)
+    z, chunks = c.wola_fd_broadcast(x, p.Nh, p.R, w)
 
     # Run TD-WOLA "equivalent"
-    z2 = c.wola_td_broadcast(x, p.Nh, p.R, w)
+    ztd = c.wola_td_broadcast(x, p.Nh, w, updateFilterEveryXsamples=1000)
+    # Subdivide in blocks and go to freq. domain
+    nchunks = z.shape[1] - 1
+    zchunkedtd = np.zeros((p.Nh, nchunks))
+    zchunked = np.zeros((p.Nh, nchunks), dtype=complex)
+    for ii in range(nchunks):
+        zchunkedtd[:, ii] = copy.copy(ztd[(ii * p.R):(ii * p.R + p.Nh)])
+        zchunked[:, ii] = np.fft.fft(zchunkedtd[:, ii] * np.sqrt(np.hanning(p.Nh)), p.Nh, axis=0)
+
+    # import simpleaudio as sa
+    # import time
+    # audio_array = x * 32767 / max(abs(x))
+    # audio_array = audio_array.astype(np.int16)
+    # sa.play_buffer(audio_array,1,2,fs)
+    # time.sleep(p.T)
+    # audio_array = ztd * 32767 / max(abs(ztd))
+    # audio_array = audio_array.astype(np.int16)
+    # sa.play_buffer(audio_array,1,2,fs)
+
+    
+    # import matplotlib.pyplot as plt
+    # fig, axes = plt.subplots(1,1)
+    # fig.set_size_inches(8.5, 3.5)
+    # # axes.plot(ztd / np.amax(ztd))
+    # # axes.plot(x / np.amax(x))
+    # # axes[0].plot(chunks.T / np.amax(chunks))
+    # # axes[1].plot(zchunkedtd / np.amax(zchunkedtd))
+    # # axes.plot(20*np.log10(np.abs(zchunked)))
+    # # axes.plot(20*np.log10(np.abs(z)))
+    # # axes.grid()
+    # plt.tight_layout()	
+    # plt.show()
 
     # Plot
-    c.plotit(x, y, w)
+    c.plotit(z, zchunked, 100, unwrapPhase=True)
+
+    stop = 1
 
 
 # ------------------------------------ RUN SCRIPT ------------------------------------
