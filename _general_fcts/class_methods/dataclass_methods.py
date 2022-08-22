f83b424eb0f22983edf112c8ae32890c8ba47ebe
p-didier <paul.didier@esat.kuleuven.be>
2022-08-22 13:08:48 +0200

    Export/import of dataclasses from/to JSON

--- _general_fcts/class_methods/dataclass_methods.py
+++ _general_fcts/class_methods/dataclass_methods.py
@@ -2,6 +2,19 @@ import pickle, gzip
 from pathlib import Path
 from dataclasses import fields
 from pathlib import PurePath
+import dataclasses, json, os
+
+class EnhancedJSONEncoder(json.JSONEncoder):  # https://stackoverflow.com/a/51286749
+        def default(self, o):
+            if dataclasses.is_dataclass(o):
+                return dataclasses.asdict(o)
+            return super().default(o)
+class EnhancedJSONDecoder(json.JSONDecoder):  # https://stackoverflow.com/a/51286749
+        def default(self, o):
+            if dataclasses.is_dataclass(o):
+                return dataclasses.asdict(o)
+            return super().default(o)
+
 
 def save(self, foldername: str):
     """Saves program settings so they can be loaded again later"""
@@ -23,6 +36,7 @@ def load(self, foldername: str, silent=False):
         print(f'<{type(self).__name__}> object data loaded from directory\n".../{shortPath}".')
     return p
 
+
 def save_as_txt(self, filename):
     if filename[-4:] != '.txt':
         if filename[-1] != '/':
@@ -34,6 +48,39 @@ def save_as_txt(self, filename):
     for ii in range(len(flds)):
         string = f'Field "{flds[ii][0]}" = {flds[ii][1]}\n'
         f.write(string)
+
+
+def save_to_json(mycls, filename):
+    """Saves dataclass to JSON file"""
+
+    # Check extension
+    filename_alone, file_extension = os.path.splitext(filename)
+    if file_extension != '.json':
+        print(f'The filename ("{filename}") should be that of a JSON file. Modifying...')
+        filename = filename_alone + '.json'
+        print(f'Filename modified to "{filename}".')
+
+    jsonstr = json.dumps(mycls, cls=EnhancedJSONEncoder)  # https://stackoverflow.com/a/51286749
+    with open(filename, 'w') as file_json:
+        file_json.write(jsonstr)
+    file_json.close()
+
+
+def load_from_json(path_to_json_file, mycls):
+    """Loads dataclass from JSON file"""
+
+    # Check extension
+    filename_alone, file_extension = os.path.splitext(path_to_json_file)
+    if file_extension != '.json':
+        print(f'The filename ("{path_to_json_file}") should be that of a JSON file. Modifying...')
+        path_to_json_file = filename_alone + '.json'
+        print(f'Filename modified to "{path_to_json_file}".')
+
+    fp = open(path_to_json_file)
+    json_file_as_dict = json.load(fp, cls=EnhancedJSONDecoder)
+    mycls_out = mycls(**json_file_as_dict)
+
+    return mycls_out
 
 
 def shorten_path(file_path, length=3):
