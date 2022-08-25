188b5a104a69fdaaff0d9d49b8c4d048d5f5a08e
p-didier <paul.didier@esat.kuleuven.be>
2022-08-25 10:30:11 +0200

    Latest plot version as of 20220824

--- 03_plots/20220823_SRO_comp/script.py
+++ 03_plots/20220823_SRO_comp/script.py
@@ -3,16 +3,18 @@ import json
 import sys, os
 import numpy as np
 import matplotlib.pyplot as plt
+from pathlib import Path
 # import dataclass_wizard as dcw
 
 PATHTORESULTS = '01_algorithms/01_NR/02_distributed/res/testing_SROs/automated/20220822_SROestComp_wFlags/J2Mk[1_1]_Ns1_Nn1'
 RESULTSFILENAME = 'Results.json'
 SETTINGSFILENAME = 'ProgramSettings.json'
+EXPORTFIGURE = 1
 
 @dataclass
 class Options:
     metricsToPlot: list[str]    # speech enhancement metrics to be plotted
-    valuesToPlot: list[str]     # type of values to be plotted (e.g., "before" for before enhancement, "after", "diff", "diffLocal", etc.)
+    valuesToPlot: str     # type of values to be plotted (e.g., "before" for before enhancement, "after", "diff", "diffLocal", etc.)
     compOptions: list[str] = field(default_factory=list)
     singleNode: list = field(default_factory=list)
     nodeIndices: np.ndarray = np.array([])
@@ -22,15 +24,22 @@ def main():
     opts = Options(
         # metricsToPlot=['stoi', 'pesq', 'fwSNRseg'],
         metricsToPlot=['stoi', 'fwSNRseg'],
-        # valuesToPlot=['diff'],
-        valuesToPlot=['after'],
+        valuesToPlot='diff',
+        # valuesToPlot=['after'],
         compOptions=['Comp.', 'No comp.'],
-        singleNode=[True, 1]    # [yes/no, node index]
+        singleNode=[True, 0]    # [yes/no, node index [python]]
+        # singleNode=[True, 1]    # [yes/no, node index [python]]
+        # singleNode=[False, None]    # [yes/no, node index [python]]
         )
 
     sros, data = process_data(opts)
 
-    plot_data(sros, data, opts)
+    fig = plot_data(sros, data, opts)
+
+    if EXPORTFIGURE:
+        fig.savefig(f'{Path(PATHTORESULTS).parent}/figs/metricsFigure.png')
+        fig.savefig(f'{Path(PATHTORESULTS).parent}/figs/metricsFigure.pdf')
+        plt.show()
 
     return None
 
@@ -39,6 +48,7 @@ def process_data(opts: Options):
 
     # Find subfolders
     dirs = os.listdir(PATHTORESULTS)
+    dirs = [ii for ii in dirs if os.path.isdir(PATHTORESULTS + '/' + ii)]
 
     # Find number of nodes
     idx = PATHTORESULTS.rfind('/')
@@ -49,7 +59,6 @@ def process_data(opts: Options):
     # Init dictionary
     res = dict.fromkeys(dirs, None)
     for dir in dirs:
-
         subdirs = os.listdir(PATHTORESULTS + '/' + dir)
 
         # Find all SRO values from subdirectory names
@@ -69,8 +78,7 @@ def process_data(opts: Options):
             for ii in opts.nodeIndices:
                 res[dir][opts.compOptions[aa]][f'N{ii + 1}'] = dict.fromkeys(opts.metricsToPlot, None)
                 for jj in range(len(opts.metricsToPlot)):
-                    res[dir][opts.compOptions[aa]][f'N{ii + 1}'][opts.metricsToPlot[jj]] = \
-                        dict.fromkeys(opts.valuesToPlot, np.full(len(sros), fill_value=None))
+                    res[dir][opts.compOptions[aa]][f'N{ii + 1}'][opts.metricsToPlot[jj]] = np.full(len(sros), fill_value=None)
 
         for _, subdir in enumerate(subdirs):
             # Load results file
@@ -93,14 +101,13 @@ def process_data(opts: Options):
 
             for ii in opts.nodeIndices:
                 for jj in range(len(opts.metricsToPlot)):
-                    for kk in range(len(opts.valuesToPlot)):
-                        res[dir][currCompOption][f'N{ii + 1}'][opts.metricsToPlot[jj]][opts.valuesToPlot[kk]][idxSrosList] = \
-                            d['enhancementEval'][opts.metricsToPlot[jj]][f'Node{ii + 1}'][opts.valuesToPlot[kk]]
+                    res[dir][currCompOption][f'N{ii + 1}'][opts.metricsToPlot[jj]][idxSrosList] = \
+                        d['enhancementEval'][opts.metricsToPlot[jj]][f'Node{ii + 1}'][opts.valuesToPlot]
 
-                        # Edge case: SRO = 0 PPM -- not computed: simply copy "Comp." value to "No comp." entry
-                        if currCompOption == 'No comp.' and currSro == 0:
-                            res[dir]['Comp.'][f'N{ii + 1}'][opts.metricsToPlot[jj]][opts.valuesToPlot[kk]][idxSrosList] = \
-                                d['enhancementEval'][opts.metricsToPlot[jj]][f'Node{ii + 1}'][opts.valuesToPlot[kk]]
+                    # Edge case: SRO = 0 PPM -- not computed: simply copy "Comp." value to "No comp." entry
+                    if currCompOption == 'No comp.' and currSro == 0:
+                        res[dir]['Comp.'][f'N{ii + 1}'][opts.metricsToPlot[jj]][idxSrosList] = \
+                            d['enhancementEval'][opts.metricsToPlot[jj]][f'Node{ii + 1}'][opts.valuesToPlot]
 
     return sros, res
 
@@ -112,31 +119,44 @@ def plot_data(sros, data: dict, opts: Options):
     dirs = list(data.keys())
 
     fig, axes = plt.subplots(1, nSubplots)
-    fig.set_size_inches(6.5, 2.5)
+    fig.set_size_inches(8.5, 4.5)
     for ii in range(nSubplots):
         for jj in range(len(dirs)): 
             for aa in range(len(opts.compOptions)):
-                for k in opts.nodeIndices:
-                    for kk in range(len(opts.valuesToPlot)):
-                        toPlot = data[dirs[jj]][opts.compOptions[aa]][f'N{k + 1}'][opts.metricsToPlot[ii]][opts.valuesToPlot[kk]]
-                        lab = f'{dirs[jj]} - N{k + 1} - {opts.compOptions[aa]}'
-                        if 'No' in opts.compOptions[aa]:
-                            style = f'C{jj}.--'
-                        else:
-                            style = f'C{jj}.-'
-                        axes[ii].plot(sros, toPlot, style, label=lab)
+                for idxk, k in enumerate(opts.nodeIndices):
+                    toPlot = data[dirs[jj]][opts.compOptions[aa]][f'N{k + 1}'][opts.metricsToPlot[ii]]
+                    
+                    lab = f'{dirs[jj]} - N{k + 1} - {opts.compOptions[aa]}'
+                    if 'No' in opts.compOptions[aa]:
+                        style = f'C{jj}.--'
+                    else:
+                        style = f'C{jj}.-'
+                    axes[ii].plot(sros, toPlot, style, label=lab, alpha=1/(idxk + 1))
         axes[ii].grid()
         axes[ii].set_xlabel('SRO $\\varepsilon_{{kq}}$ [ppm]')
         if opts.metricsToPlot[ii] == 'stoi':
-            axes[ii].set_ylim([0, 1])
-            axes[ii].set_title(f'eSTOI -- {opts.valuesToPlot[kk]}')
-        if opts.metricsToPlot[ii] == 'pesq':
-            axes[ii].set_title(f'PESQ -- {opts.valuesToPlot[kk]}')
-        if opts.metricsToPlot[ii] == 'fwSNRseg':
-            axes[ii].set_title(f'fwSNRseg -- {opts.valuesToPlot[kk]}')
-    axes[0].legend(loc='lower left')
-    # plt.tight_layout()
-    plt.show()
+            tistr = 'eSTOI'
+        elif opts.metricsToPlot[ii] == 'pesq':
+            tistr = 'PESQ'
+        elif opts.metricsToPlot[ii] == 'fwSNRseg':
+            tistr = 'fwSNRseg'
+        if opts.valuesToPlot == 'diff':
+            tistr += ' - $\\Delta$ before/after enhancement'
+        elif opts.valuesToPlot == 'after':
+            tistr += ' - metric after enhancement'
+        elif opts.valuesToPlot == 'before':
+            tistr += ' - metric before enhancement'
+        axes[ii].set_title(tistr)
+        if (toPlot < 0).any():
+            axes[ii].hlines(y=0, xmin=0, xmax=np.amax(sros), colors='k', linestyles='--')
+    plt.tight_layout()
+    # axes[0].legend(loc='lower left')
+    axes[0].legend(loc='best')
+
+    if not EXPORTFIGURE:
+        plt.show()
+
+    return fig
 
 
 if __name__ == '__main__':
