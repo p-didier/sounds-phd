10bd8e12b9fb31572305119000f0f53c7a7a1f27
p-didier <paul.didier@esat.kuleuven.be>
2022-11-04 12:01:33 +0100

    Functional inferring at first degree of connection -- attempt to go beyond first round

--- 97_tests/02_toolboxes_testing/04_networkx/script.py
+++ 97_tests/02_toolboxes_testing/04_networkx/script.py
@@ -1,6 +1,6 @@
 import networkx as nx
 import numpy as np
-import sys
+import sys, copy
 import matplotlib.pyplot as plt
 from scipy.spatial import distance_matrix
 
@@ -8,6 +8,10 @@ NNODES = 10
 CONNECTIONDIST = 2  # maximum distance for two nodes to be able to communicate
 SQUAREROOMDIM = 5
 SEED = 12335
+COLORDEFAULTLINK = 'k'
+COLORLEAFLINK = 'g'
+COLORBESTSNRLINK = 'r'
+COLORINFERENCELINK = 'b'
 
 np.random.seed(SEED)
 
@@ -27,8 +31,6 @@ def main():
     np.fill_diagonal(connMat, 1)
     # Create adjacency dictionary
     adjdict = _get_adjacency_dict(connMat)
-    # List leaf nodes
-    leaves =  np.array([ii for ii in range(NNODES) if len(adjdict[ii]) == 1])
 
     # Generate directed graph
     myG = nx.DiGraph(adjdict) 
@@ -40,62 +42,14 @@ def main():
     SNRs = 1 / distance_matrix(posNodes, sourcePos)  # distance (SNR-like)
 
     # Derive edge colors and directions
-    edges = np.array(myG.edges)
-    edgecolors = ['k'] * len(edges)
-    for k in range(NNODES):
-        edgeidx = np.array([h for h in range(len(edges)) if k in edges[h]])
-        # Order indices
-        firstNodeidx = [ii for ii in range(len(edgeidx)) if edges[edgeidx[ii]][0] == k]
-        secondNodeidx = [ii for ii in range(len(edgeidx)) if edges[edgeidx[ii]][1] == k]
-        reorderingIdx = np.concatenate((firstNodeidx, secondNodeidx))
-        edgeidx = edgeidx[reorderingIdx]
+    edgecolors = get_graph_colors(myG, adjdict, SNRs)
 
-        # Non-leaf nodes
-        if k not in leaves:
-            # List neighbors
-            stemNeighs = np.array(adjdict[k])
-            for neighIdx, q in enumerate(stemNeighs):
-                if q not in leaves:
-                    if SNRs[k] <= SNRs[q]:
-                        passiveEdgeIdx = edgeidx[neighIdx]
-                        activeEdgeIdx = edgeidx[neighIdx + len(edgeidx) // 2]
-                    else:
-                        passiveEdgeIdx = edgeidx[neighIdx + len(edgeidx) // 2]
-                        activeEdgeIdx = edgeidx[neighIdx]
-                    edgecolors[passiveEdgeIdx] = (0,0,0,0)
-                    edgecolors[activeEdgeIdx] = 'r'
-        # Leaf nodes
-        else:
-            for jj in range(len(edgeidx) // 2):
-                # vvv make outgoing arrow transparent
-                edgecolors[edgeidx[len(edgeidx) - jj - 1]] = 'g'
-                # vvv make incoming arrow green
-                edgecolors[edgeidx[jj]] = (0,0,0,0)
-            
-
-    # # Loop through nodes attached to leaves
-    # stemsReached = []
-    # for ii in range(len(leaves)):
-    #     stemNode = adjdict[leaves[ii]][0]
-    #     neighbors = np.array(adjdict[stemNode])
-    #     # vvv only consider non-leaf neighbors
-    #     neighbors = neighbors[neighbors != leaves[ii]]
-    #     if all(SNRs[stemNode] > SNRs[neighbors]):  # best node among neighbors
-    #         idx = np.array([k for k in range(len(edges))
-    #             if stemNode in edges[k]])
-    #         edgecolors[idx] = 'r'
-    #     stemsReached.append(stemNode)
-    # edgecolors[leaves] = 'g'  # leaf nodes infer
-    # # Loop through remaining nodes
-    # stemNodes = [ii for ii in np.arange(NNODES)\
-    #     if ii not in leaves and ii not in stemsReached]
-    # for ii in range(len(stemNodes)):
-    #     currStem = stemNodes[ii]
-    #     # vvv only consider neighbors that have not been considered yet
-    #     neighbors = np.array([_ for _ in adjdict[stemNode] if _ in stemNodes])
-
-        
-
+    # Create new graph based on remaining edges
+    remainingEdges = np.array(myG.edges)[np.array(edgecolors) == COLORDEFAULTLINK]
+    remainingEdges = [tuple(ii) for ii in remainingEdges]
+    myGnew = nx.DiGraph(remainingEdges)
+    adjdictnew = {ii : tuple(myGnew.adj[ii]) for ii in list(myGnew.nodes)}
+    edgecolors2 = get_graph_colors(myGnew, adjdictnew, SNRs[list(myGnew.nodes)])
 
     # Plot
     axes = plot_network(myG, layout, edgecolors)
@@ -104,6 +58,100 @@ def main():
     plt.show()
 
     stop = 1
+
+
+def get_graph_colors(myG, adjdict, SNRs):
+    """Compute the edge colors of a directed graph according to SRO
+    propagation (and redundancy reduction) rules."""
+    
+    edges = np.array(myG.edges)
+    edgecolors = [COLORDEFAULTLINK] * len(edges)
+
+    nodes = list(myG.nodes)
+
+    # List leaf nodes
+    leaves =  np.array([ii for ii in nodes if len(adjdict[ii]) == 1])
+
+    for k in nodes:
+        edgeidx = np.array([h for h in range(len(edges)) if k in edges[h]])
+        if len(edgeidx) == 0:
+            continue
+        # Order indices
+        firstNodeidx = [ii for ii in range(len(edgeidx)) if edges[edgeidx[ii]][0] == k]
+        secondNodeidx = [ii for ii in range(len(edgeidx)) if edges[edgeidx[ii]][1] == k]
+        reorderingIdx = np.concatenate((firstNodeidx, secondNodeidx))
+        edgeidx = edgeidx[reorderingIdx]
+
+        # Non-leaf nodes with best SNRs
+        bestSNRnodes = []
+        if k not in leaves:
+            # List neighbors
+            stemNeighs = np.array(adjdict[k])
+            if all(SNRs[k] >= SNRs[stemNeighs]):
+                bestSNRnodes.append(k)
+                passiveEdgeIdx = edgeidx[np.arange(len(stemNeighs)) + len(edgeidx) // 2]
+                activeEdgeIdx = edgeidx[np.arange(len(stemNeighs))]
+                for ii in range(len(passiveEdgeIdx)):
+                    edgecolors[passiveEdgeIdx[ii]] = (0,0,0,0)
+                for ii in range(len(activeEdgeIdx)):
+                    edgecolors[activeEdgeIdx[ii]] = COLORBESTSNRLINK
+        # Leaf nodes
+        else:
+            for jj in range(len(edgeidx) // 2):
+                # vvv make outgoing arrow transparent
+                edgecolors[edgeidx[len(edgeidx) - jj - 1]] = COLORLEAFLINK
+                # vvv make incoming arrow green
+                edgecolors[edgeidx[jj]] = (0,0,0,0)
+        
+    # Consider other non-best-SNR nodes
+    for k in nodes:
+        if k not in bestSNRnodes and k not in leaves:
+            # List neighbors
+            neighbors = np.array(adjdict[k])
+            # For each neighbor, check adjacency
+            for ii in range(len(neighbors)):
+                if neighbors[ii] in bestSNRnodes or neighbors[ii] in leaves:
+                    pass
+                else:
+                    # Check if `k` and `neighbors[ii]` have common neighbors.
+                    commonNeighs = _common_neighbor(k, neighbors[ii], adjdict)
+                    # If they do, check whether they know their respective 
+                    # SROs with those common neighbors.
+                    for jj in range(len(commonNeighs)):
+                        idx1 = _eidx(k, commonNeighs[jj], edges)
+                        idx2 = _eidx(neighbors[ii], commonNeighs[jj], edges)
+                        if edgecolors[idx1] not in [COLORDEFAULTLINK, COLORINFERENCELINK]\
+                            and edgecolors[idx2] not in [COLORDEFAULTLINK, COLORINFERENCELINK]:
+                            # vvv if they do, make both arrows blue
+                            idx11 = _eidx(k, neighbors[ii], edges)
+                            idx12 = _eidx(neighbors[ii], k, edges)
+                            edgecolors[idx11] = COLORINFERENCELINK
+                            edgecolors[idx12] = COLORINFERENCELINK
+                            break
+
+    return edgecolors
+
+
+def _eidx(k,q,edges):
+    """Returns boolean array to select the index of (directed) edge
+    from `k` to `q`."""
+    allidx = np.arange(len(edges))
+    myidx = allidx[np.logical_and(edges[:, 0] == k, edges[:, 1] == q)]
+    if len(myidx) > 1:
+        raise ValueError('Issue here.')
+    elif len(myidx) == 0:
+        return None
+    return myidx[0]
+
+
+def _common_neighbor(k, q, adj):
+    """Checks whether node `k` and `q` have a common neighbor."""
+    neighs_k, neighs_q = adj[k], adj[q]
+    commonNeighbors = []
+    for ii in range(len(neighs_k)):
+        if neighs_k[ii] != q and neighs_k[ii] in neighs_q:
+            commonNeighbors.append(neighs_k[ii])
+    return commonNeighbors
 
 
 def plot_network(G, layout, edge_color=None):
