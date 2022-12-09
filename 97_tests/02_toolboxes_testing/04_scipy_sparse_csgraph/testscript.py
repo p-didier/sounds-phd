8400ca6fe66e44ce0425f1573688cb09e5a1c81a
p-didier <paul.didier@esat.kuleuven.be>
2022-12-09 14:54:19 +0100

    Fully functional MST-formation-algorithm comparison script (incl. MC runs)

--- 97_tests/02_toolboxes_testing/04_scipy_sparse_csgraph/testscript.py
+++ 97_tests/02_toolboxes_testing/04_scipy_sparse_csgraph/testscript.py
@@ -2,43 +2,113 @@ import sys, time
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
+from pathlib import Path
 
-NNODES = 20
-SEED = 12345
-NETTYPE = 'fullyconnected'
+# Comparison between NetworkX's Minimum Spanning Tree (MST) formation
+# algorithms.
+
+SEED = 12345  # random generators seed
+NNODES = np.arange(5, stop=21)
+NMC_PERNNODES = 50  # number of Monte-Carlo runs per number of nodes
+ALGOS = ['kruskal', 'prim', 'boruvka']  # MST formation algorithms to consider
+# NETTYPE = 'fullyconnected'
 NETTYPE = 'adhoc'
 
+
 def main():
-    """Main function"""
+    """Main function."""
 
-    # Generate original graph
-    if NETTYPE == 'fullyconnected':
-        OGnx = nx.complete_graph(NNODES)
-    elif NETTYPE == 'adhoc':
-        OGnx = nx.random_geometric_graph(NNODES, radius=0.5, seed=SEED)
-    else:
-        raise ValueError(f'Network type "{NETTYPE}" not understood.')
-    # Generate node positions
-    pos = nx.spring_layout(OGnx, seed=SEED)
-    pos = nx.kamada_kawai_layout(OGnx)
-    # Add edge weights
-    for e in OGnx.edges():
-        # Compute weight based on distance
-        weight = np.linalg.norm(pos[e[0]] - pos[e[1]])
-        OGnx[e[0]][e[1]]['weight'] = weight
+    # vvvv Value format: [mean, standard deviation] across MC runs
+    allTimings = dict([
+        (n, dict([
+            (algo, [None, None]) for algo in ALGOS
+        ])) for n in NNODES
+    ])
+    for idxNnodes in range(len(NNODES)):
+        nNodes = NNODES[idxNnodes]
+        timingsCurrNnodes = dict([(algo, []) for algo in ALGOS])
+        for idxMC in range(NMC_PERNNODES):
+            # Inform user
+            print(f'{nNodes} nodes -- MC sim. {idxMC + 1}/{NMC_PERNNODES}...')
+            # Generate original graph
+            if NETTYPE == 'fullyconnected':
+                OGnx = nx.complete_graph(nNodes)
+            elif NETTYPE == 'adhoc':
+                OGnx = nx.random_geometric_graph(nNodes, radius=0.5, seed=SEED)
+            else:
+                raise ValueError(f'Network type "{NETTYPE}" not understood.')
+            # Generate node positions
+            pos = nx.spring_layout(OGnx, seed=SEED)
+            pos = nx.kamada_kawai_layout(OGnx)
+            # Add edge weights
+            for e in OGnx.edges():
+                # Compute weight based on distance
+                weight = np.linalg.norm(pos[e[0]] - pos[e[1]])
+                OGnx[e[0]][e[1]]['weight'] = weight
 
-    # Compute MSTs (using the three available algorithms)
-    algos = ['kruskal', 'prim', 'boruvka']
-    MSTs, timings = get_msts(OGnx, algos)
+            # Compute MSTs (using the three available algorithms)
+            MSTs, timings = get_msts(OGnx, ALGOS)
 
-    # Plot
-    plot_msts(OGnx, MSTs, pos, timings, nodeSize=100)
+            # Plot
+            if 0:
+                plot_msts(OGnx, MSTs, pos, timings, nodeSize=100)
+                plt.show()
+            
+            # Aggregate timing results for current number of nodes
+            for algo in ALGOS:
+                timingsCurrNnodes[algo].append(timings[algo])
+        
+        # Aggregate global timing information
+        for algo in ALGOS:
+            allTimings[nNodes][algo][0] = np.mean(timingsCurrNnodes[algo])
+            allTimings[nNodes][algo][1] = np.std(timingsCurrNnodes[algo])
+
+    # Show results
+    fig = final_plot(allTimings)
     plt.show()
+    if 1:
+        fig.savefig(f'{Path(__file__).parent}/figs/timing_comp.png')
+        fig.savefig(f'{Path(__file__).parent}/figs/timing_comp.pdf')
 
     stop = 1
 
+
+def final_plot(timings: dict):
+    """Timing-comparison performance plot."""
+
+    miniDelta = 0.05
+    legHandles = np.zeros(len(timings[list(timings.keys())[0]].keys()), dtype=object)
+
+    fig, axes = plt.subplots(1, 1)
+    fig.set_size_inches(8, 4)
+    for ii, nNodesStr in enumerate(timings.keys()):    # for each number of nodes considered
+        for jj, algo in enumerate(timings[nNodesStr].keys()):  # for each algorithm considered
+            obj = axes.errorbar(
+                x=ii + miniDelta * jj,
+                y=1e3 * timings[nNodesStr][algo][0],
+                yerr=1e3 * timings[nNodesStr][algo][1],
+                marker='o',
+                color=f'C{jj}',
+                ecolor=f'C{jj}',
+            )
+            legHandles[jj] = obj.lines[0]
+    axes.grid()
+    axes.set_xticks(np.arange(len(timings.keys())))
+    axes.set_xticklabels(list(timings.keys()))
+    axes.set_xlabel('Number of nodes')
+    axes.set_ylabel('[ms]')
+    axes.set_title(f'MST algs. timings ({NMC_PERNNODES} MCs / # of nodes)')
+    axes.legend(
+        legHandles,
+        [f'Algo: {algo}' for algo in timings[list(timings.keys())[0]].keys()]
+    )
+
+    return fig
+
+
 def get_msts(G, algos):
     """Compute MSTs using the listed algorithms."""
+
     timings = dict([(algo, None) for algo in algos])
     msts = dict([(algo, None) for algo in algos])
     for algo in algos:
@@ -55,6 +125,7 @@ def get_msts(G, algos):
 
 def plot_msts(oggraph, msts: dict, pos, timings, nodeSize=100):
     """Plot MST-formation algorithms' output."""
+
     algos = list(msts.keys())
     nAlgos = len(algos)
 
@@ -64,8 +135,17 @@ def plot_msts(oggraph, msts: dict, pos, timings, nodeSize=100):
     nx.draw(oggraph, pos=pos, ax=axes[0], node_size=nodeSize)
     axes[0].set_title(f'Original graph (type: "{NETTYPE}")')
     for ii, algo in enumerate(algos):
-        nx.draw(msts[algo], pos=pos, ax=axes[ii + 1], edge_color='r', width=2.0, node_size=nodeSize)
-        axes[ii + 1].set_title(f"{algo}'s MST ({np.round(timings[algo], 4)} s)")
+        nx.draw(
+            msts[algo],
+            pos=pos,
+            ax=axes[ii + 1],
+            edge_color='r',
+            width=2.0,
+            node_size=nodeSize
+        )
+        axes[ii + 1].set_title(
+            f"{algo}'s MST ({np.round(timings[algo], 4)} s)"
+        )
 
 
 if __name__ == '__main__':
