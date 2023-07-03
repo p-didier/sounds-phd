781bf87c272c610f71f9f861a332a3273e20c076
p-didier <paul.didier@esat.kuleuven.be>
2023-07-03 12:57:31 +0200

    Match between MWF and matched BF + spectral post-filter

--- 97_tests/06_pure_linalg/20230630_rank1model/script.py
+++ 97_tests/06_pure_linalg/20230630_rank1model/script.py
@@ -4,56 +4,219 @@
 # (c) Paul Didier, SOUNDS ETN, KU Leuven ESAT STADIUS
 
 import sys
+import resampy
+import scipy.linalg as la
 import numpy as np
 import soundfile as sf
 import matplotlib.pyplot as plt
 
 TARGET_SIGNAL = 'danse/tests/sigs/01_speech/speech2_16000Hz.wav'
-N_SENSORS = 2
-RIR_LENGTH = 1000
-SELFNOISE_FACTOR = 0.05
-DURATION = 5  # seconds
+N_SENSORS = 10
+SELFNOISE_FACTOR = 0.5
+DURATIONS = np.logspace(np.log10(1), np.log10(30), 50)
+# DURATIONS = [20]
+FS = 16e3
+N_MC = 10
 
-def main():
+# FILTER_TYPE = 'mwf'  # 'mwf' or 'gevdmwf'
+FILTER_TYPE = 'gevdmwf'  # 'mwf' or 'gevdmwf'
+
+SEED = 0
+
+def main(
+        M=N_SENSORS,
+        durations=DURATIONS,
+        fs=FS,
+        nMC=N_MC,
+        selfNoiseFactor=SELFNOISE_FACTOR,
+        seed=SEED
+    ):
     """Main function (called by default when running script)."""
-    
-    # Load target signal
-    targetSignal, fs = sf.read(TARGET_SIGNAL)
-    targetSignal = targetSignal[:int(DURATION * fs)]
-    
-    # Generate clean signals
-    mat = np.tile(targetSignal, (3, 1)).T
-    cleanSigs = mat @ np.diag([1, 0.75, 0.5])  # rank-1 matrix (scalings only)
-    r = np.linalg.matrix_rank(cleanSigs.T @ cleanSigs)
-    print(f'Rank of clean-signals matrix Rss: {r}')
 
-    # # Generate RIRs
-    # rirs = np.zeros((RIR_LENGTH, N_SENSORS))
-    # for n in range(N_SENSORS):
-    #     rir = np.zeros(RIR_LENGTH)
-    #     # rir[np.random.randint(low=0, high=RIR_LENGTH // 2, dtype=int)] =\
-    #     #     np.random.uniform(low=0.5, high=1)
-    #     rir[0] = np.random.uniform(low=0.5, high=1)
-    #     # rir[0] = 1
-    #     rirs[:, n] = rir
+    # Set random seed
+    np.random.seed(seed)
 
-    # Generate noisy signals
-    noisySignals = np.zeros((len(targetSignal), N_SENSORS))
-    for n in range(N_SENSORS):
-        noisySignals[:, n] = cleanSigs[:, n] + SELFNOISE_FACTOR *\
-            np.random.uniform(low=-1, high=1, size=len(targetSignal))
-    
-    r = np.linalg.matrix_rank(noisySignals.T @ noisySignals)
-    print(f'Rank of noisy-signals matrix Ryy: {r}')
-    
-    # # Plot signals
-    # fig, axs = plt.subplots(1, 1)
-    # for n in range(N_SENSORS):
-    #     axs.plot(noisySignals[:, n], label=f'Sensor {n}')
-    # plt.legend()
-    # plt.show()
+    diff = np.zeros((nMC, len(durations)))
+    # scalings = np.random.uniform(low=50, high=100, size=M)
+    scalings = np.random.uniform(low=0.5, high=1, size=M)
+    # Get clean signals
+    nSamples = int(np.amax(durations) * fs)
+    cleanSigs, latentSignal = get_clean_signals(M, np.amax(durations), scalings, fs)
+    sigma_sr = np.sqrt(np.mean(latentSignal ** 2))
+    for idxMC in range(nMC):
+        print(f'Running Monte-Carlo iteration {idxMC+1}/{nMC}')
+
+        # Generate noise signals
+        noiseSignals = np.zeros((nSamples, M))
+        sigma_nr = np.zeros(M)
+        for n in range(M):
+            noiseSignals[:, n] = selfNoiseFactor * np.random.uniform(
+                low=-1,
+                high=1,
+                size=nSamples
+            )
+            sigma_nr[n] = np.sqrt(np.mean(noiseSignals[:, n] ** 2))
+            
+        # Loop over durations
+        for ii in range(len(durations)):
+            # Generate noisy signals
+            nSamples = int(durations[ii] * fs)
+            noisySignals = cleanSigs[:nSamples, :] + noiseSignals[:nSamples, :]
+
+            # MWF
+            filter = compute_filter(
+                noisySignals,
+                cleanSigs[:nSamples, :],
+                noiseSignals[:nSamples, :],
+                type=FILTER_TYPE
+            )
+
+            # Compute difference between normalized estimated filters
+            # and normalized expected filters
+            diffsPerSensor = np.zeros(M)
+            for n in range(M):
+                rtf = scalings / scalings[n]
+                hs = np.sum(rtf ** 2) * sigma_sr ** 2
+                spf = hs / (hs + sigma_nr[n] ** 2)  # spectral post-filter
+                fasAndSPF = rtf / (rtf.T @ rtf) * spf  # FAS BF + spectral post-filter
+                diffsPerSensor[n] = np.mean(np.abs(filter[:, n] - fasAndSPF))
+            diff[idxMC, ii] = np.mean(diffsPerSensor)
+
+        # Plots
+        if 1:
+            filteredSignals = np.zeros((nSamples, M))
+            filteredSignal_RTFs = np.zeros((nSamples, M))
+            for n in range(M):
+                filteredSignals[:, n] = noisySignals @ filter[:, n]
+                h = scalings / scalings[n]
+                hs = np.sum(h ** 2) * sigma_sr ** 2
+                spf = hs / (hs + sigma_nr[n] ** 2)
+                filteredSignal_RTFs[:, n] = noisySignals @\
+                    h / (h.T @ h) * spf
+            # plot_results(
+            #     cleanSigs[:nSamples, :],
+            #     noisySignals,
+            #     filteredSignals,
+            #     filteredSignal_RTFs
+            # )
+            plot_filter(filter, scalings, sigma_sr, sigma_nr)
+            stop = 1
+
+    # Plot difference
+    plt.figure()
+    plt.loglog(durations, diff.T, '-', color='0.75')
+    plt.loglog(durations, np.mean(diff, axis=0), '.-', color='k')
+    plt.grid(which='both')
+    plt.xlabel('Signal duration (s)')
+    plt.ylabel(f'Abs. diff. bw. estimated and expected filter ({nMC} MC runs)')
+    plt.show(block=False)
+
 
     stop = 1
+
+
+def plot_filter(filter, scalings, sigma_sr, sigma_nr):
+    fig, axs = plt.subplots(1, 1)
+    for n in range(filter.shape[1]):
+        # axs.plot(filter[:, n] * np.sum(scalings ** 2), '.-', label=f'MWF weights sensor {n}')
+        axs.plot(filter[:, n], f'C{n}.-', label=f'MWF weights sensor {n}')
+        # axs.plot(filter[:, n] / np.amax(np.abs(filter[:, n])), '.-', label=f'MWF weights sensor {n}')
+        h = scalings / scalings[n]
+        hs = np.sum(h ** 2) * sigma_sr ** 2
+        spectralPostFilter = hs / (hs + sigma_nr[n] ** 2)
+        if n == 0:
+            axs.plot(h / np.sum(h ** 2) * spectralPostFilter, f'C{n}.--', label='Matched BF and spectral post-filter')
+        else:
+            axs.plot(h / np.sum(h ** 2) * spectralPostFilter, f'C{n}.--')
+    # axs.plot(scalings / np.amax(np.abs(scalings)), '.--', label='Signal scalings')
+    # axs.hlines(1, 0, filter.shape[0] - 1, color='k', linestyle='--', linewidth=0.5)
+    fig.legend()
+    plt.show(block=False)
+
+
+def plot_results(cleanSigs, noisySignals, filteredSignals, filteredSignal_RTFs):
+    # Plot
+    nSensors = cleanSigs.shape[1]
+    nRows = int(np.floor(np.sqrt(nSensors)))
+    nCols = int(np.ceil(nSensors / nRows))
+    fig, axs = plt.subplots(nRows, nCols, sharex=True, sharey=True)
+    delta = 2 * np.amax(np.abs(noisySignals))
+    for n in range(nSensors):
+        if nRows == 1:
+            currAx = axs[n % nCols]
+        else:
+            currAx = axs[int(np.floor(n / nCols)), n % nCols]
+        # overlay 1
+        currAx.plot(noisySignals[:, n], 'k', label=f'Noisy signal')
+        currAx.plot(filteredSignals[:, n], 'r', label=f'MWF-filtering')
+        currAx.plot(cleanSigs[:, n], 'y', label=f'Clean signal')
+        # overlay 2
+        currAx.plot(noisySignals[:, n] + delta, 'k', label=f'Noisy signal')
+        currAx.plot(filteredSignal_RTFs[:, n] + delta, 'r', label=f'RTF-filtering')
+        currAx.plot(cleanSigs[:, n] + delta, 'y', label=f'Clean signal')
+        if n == 0:
+            currAx.legend()
+        currAx.set_title(f'Sensor {n}')
+    plt.show(block=False)
+
+
+def compute_filter(
+        noisySignals: np.ndarray,
+        cleanSigs: np.ndarray,
+        noiseOnlySigs: np.ndarray,
+        type='mwf',
+        rank=1
+    ):
+    """
+    [1] Santiago Ruiz, Toon van Waterschoot and Marc Moonen, "Distributed
+    combined acoustic echo cancellation and noise reduction in wireless
+    acoustic sensor and actuator networks" - 2022
+    """
+
+    nSensors = cleanSigs.shape[1]
+    Ryy = noisySignals.T @ noisySignals
+    if type == 'mwf':
+        RyyInv = np.linalg.inv(Ryy)
+        w = np.zeros((nSensors, nSensors))
+        for n in range(nSensors):
+            Ryd = noisySignals.T @ cleanSigs[:, n]
+            w[:, n] = RyyInv @ Ryd
+    elif type == 'gevdmwf':
+        Rnn = noiseOnlySigs.T @ noiseOnlySigs
+        sigma, Xmat = la.eigh(Ryy, Rnn)
+        idx = np.flip(np.argsort(sigma))
+        sigma = sigma[idx]
+        Xmat = Xmat[:, idx]
+        Qmat = np.linalg.inv(Xmat.conj().T)
+        Dmat = np.zeros((nSensors, nSensors))
+        Dmat[:rank, :rank] = np.diag(1 - 1 / sigma[:rank])
+        w = Xmat @ Dmat @ Qmat.T.conj()   # see eq. (24) in [1]
+    return w
+
+
+def get_clean_signals(M, dur, scalings, fsTarget):
+    # Load target signal
+    latentSignal, fs = sf.read(TARGET_SIGNAL)
+    if fs != fsTarget:
+        # Resample
+        latentSignal = resampy.resample(
+            latentSignal,
+            fs,
+            fsTarget
+        )
+    # Truncate
+    latentSignal = latentSignal[:int(dur * fsTarget)]
+    # Normalize
+    # latentSignal = latentSignal / np.max(np.abs(latentSignal))
+    # Normalize power
+    latentSignal = latentSignal / np.sqrt(np.mean(latentSignal ** 2))
+    
+    # Generate clean signals
+    mat = np.tile(latentSignal, (M, 1)).T
+    # Random scalings
+    cleanSigs = mat @ np.diag(scalings)  # rank-1 matrix (scalings only)
+
+    return cleanSigs, latentSignal
 
 if __name__ == '__main__':
     sys.exit(main())
